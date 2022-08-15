ee130dd2c0bd2972c814d545a6be85acfa255df8
AMMAR AMEEN <111254049+AMMAR716@users.noreply.github.com>
2022-08-15 03:43:42 +0300

    Add files via upload

---
+++ Q5.java
@@ -0,0 +1,8 @@
+public class Q5 {
+    public static void main(String[] args) {
+        System.out.println("___________________");
+        System.out.println("| Ammar Ameen Naji |");
+        System.out.println("___________________");
+
+    }
+}
